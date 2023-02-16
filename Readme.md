440d1079be181c577fce75390946e6c4a404a87e
Federico Simonetta <fsimonetta@iccmu.es>
2023-02-16 17:59:52 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Benchamrking symbolic music features
+
+### Datasets
+
+Download the following datasets and set the paths to the root of each one in `symbolic_features/settings.py`
+
+1. [Josquin - La Rue]()
+2. [ASAP]()
+3. [Didone]()
+4. [MMD]()
+5. [EWLD]()
---
+++ symbolic_features/settings.py
@@ -0,0 +1,10 @@
+# Path to Josquin-La Rue dataset
+JLR_PATH = "datasets/mass-duos-corpus-josquin-larue"
+# Path to Asap dataset
+ASAP_PATH = "datasets/asap-dataset"
+# Path to Didone dataset
+DIDONE_PATH = "datasets/didone"
+# Path to MMD dataset
+MMD = "datasets/MMD"
+# Path to EWLD dataset
+MMD = "datasets/EWLD"
