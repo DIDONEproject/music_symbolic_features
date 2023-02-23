59ff6881947700f0cbecba180cbfb2b7c0f9f634
Federico Simonetta <fsimonetta@iccmu.es>
2023-02-23 17:35:24 +0100

    changed settings var name

--- symbolic_features/settings.py
+++ symbolic_features/settings.py
@@ -5,6 +5,6 @@ DATASETS = [f for f in os.scandir('datasets/') if f.is_dir()]
 OUTPUT = "features/"
 
 # path to the jSymbolic 2.2 jar file
-JSYMBOLIC_EXE = "./tools/jSymbolic_2_2_user/jSymbolic2.jar"
+JSYMBOLIC_JAR = "./tools/jSymbolic_2_2_user/jSymbolic2.jar"
 # path to musescore executable (could be /usr/bin/mscore, but version 3.6.2 is recommended)
-MUSESCORE = "/home/federico/bin/MuseScore-3.6.2.548021370-x86_64.AppImage"
+MSCORE_EXE = "/home/federico/bin/MuseScore-3.6.2.548021370-x86_64.AppImage"
