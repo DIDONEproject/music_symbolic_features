ed4577f4fcfda2c107d292d9a7f2be20eddbfeec
Federico Simonetta <fsimonetta@iccmu.es>
2023-03-14 18:14:15 +0100

    Fixed ewld

--- symbolic_features/data.py
+++ symbolic_features/data.py
@@ -47,7 +47,7 @@ class Dataset:
 
         df, y = self.make_label(df, label_col_selector)
 
-        idx = df[label_col_selector].str.fullmatch(self.legal_filenames)
+        idx = df[label_col_selector].str.fullmatch(self.legal_filenames).to_numpy()
         if remove_col_label is not None:
             df = df.drop(columns=label_col_selector)
         df = df.loc[idx]
@@ -57,11 +57,13 @@ class Dataset:
 
 def asap_label(df: pd.DataFrame, label_col_selector: str):
     y = df[label_col_selector].str.extract(r".*asap-dataset/(\w+)/.*", expand=False)
+    assert not y.isna().any(), "asap: NaN in y!"
     return df, y
 
 
 def didone_label(df: pd.DataFrame, label_col_selector: str):
-    y = df[label_col_selector].str.extract(r".*/xml/(Did\w+)-.*", expand=False)
+    y = df[label_col_selector].str.extract(r".*/xml/(\w+)-.*", expand=False)
+    assert not y.isna().any(), "Didone: NaN in y!"
     return df, y
 
 
@@ -78,43 +80,67 @@ def ewld_label(df: pd.DataFrame, label_col_selector: str):
     """
 
     # execute the SQL query and convert the result to a Pandas DataFrame
-    df = pd.read_sql_query(query, conn)
+    db_df = pd.read_sql_query(query, conn)
+
+    # removing path to the EWLD dataset
+    df[label_col_selector] = df[label_col_selector].str.replace(
+        str(S.DATASETS["EWLD"]) + "/", ""
+    )
+    # removing extension
+    df[label_col_selector] = df[label_col_selector].str[:-4]
+    db_df["path_leadsheet"] = db_df["path_leadsheet"].str[:-4]
+
+    # replacing strange characters
+    db_df["path_leadsheet"] = db_df["path_leadsheet"].str.replace(',', '_')
+    db_df["path_leadsheet"] = db_df["path_leadsheet"].str.replace(';', '_')
+    db_df["path_leadsheet"] = db_df["path_leadsheet"].str.replace(' ', '_')
+
+    # remove duplicates
+    db_df = db_df.groupby('path_leadsheet').first().reset_index()
 
     # select the rows in the DataFrame that match the database rows
-    matching_rows = df[df[label_col_selector].isin(df["path_leadsheet"])]
-    return matching_rows, df["genre"]
+    idx = df[label_col_selector].isin(db_df['path_leadsheet'])
+    df = df[idx]
+
+    y = db_df["genre"]
+    idx = db_df['path_leadsheet'].isin(df[label_col_selector])
+    y = y[idx]
+    assert not y.isna().any(), "EWLD: NaN in y!"
+    return df, y
 
 
 def jlr_label(df: pd.DataFrame, label_col_selector: str):
     y = df[label_col_selector].str.extract(
         r".*mass-duos-corpus-josquin-larue/(\w+)/.*", expand=False
     )
+    assert not y.isna().any(), "JLR: NaN in y!"
     return df, y
 
 
 def quartets_label(df: pd.DataFrame, label_col_selector: str):
     y = df[label_col_selector].str.extract(r".*quartets/(\w+)/.*", expand=False)
+    assert not y.isna().any(), "quartets: NaN in y!"
     return df, y
 
 
 datasets = [
-    Dataset(
-        "asap-dataset",
-        asap_label,
-        "Composer",
-        [".mid", ".xml"],
-        legal_filenames=r".+xml_score\.(?:xml|mid)",
-    ),
-    Dataset(
-        "asap-dataset",
-        asap_label,
-        "Composer",
-        [".mid"],
-        legal_filenames=r".*/[A-Z]+\w*.mid",
-        friendly_name="asap-performance",
-    ),
-    Dataset("didone", didone_label, "Aria Title", [".mid", ".xml"]),
-    Dataset("EWLD", ewld_label, "Genre", [".mid", ".xml"]),
+    # Dataset(
+    #     "asap-dataset",
+    #     asap_label,
+    #     "Composer",
+    #     [".mid", ".xml"],
+    #     legal_filenames=r".+xml_score\.(?:xml|mid)",
+    # ),
+    # Dataset(
+    #     "asap-dataset",
+    #     asap_label,
+    #     "Composer",
+    #     [".mid"],
+    #     legal_filenames=r".*/[A-Z]+\w*.mid",
+    #     friendly_name="asap-performance",
+    # ),
+    # Dataset("didone", didone_label, "Aria Title", [".mid", ".xml"]),
+    # Dataset("EWLD", ewld_label, "Genre", [".mid", ".xml"]),
     Dataset(
         "mass-duos-corpus-josquin-larue",
         jlr_label,
